e9c81037f3e794643dbef71132290b78c64bfa22
Ubuntu <ethuser@etherdev.0o3qexcj4yqujn3codutfdxvaa.bx.internal.cloudapp.net>
2020-12-23 11:34:50 +0000

    1. SmokeTest

---
+++ contracts/DappToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DappToken{
+    uint256 public totalSupply;
+    constructor() public{
+        totalSupply = 1000000;
+
+    }
+    //Set the tokens
+    //Read the total number of tokens
+}
